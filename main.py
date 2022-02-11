60f027f9caa0e1289977be99131f9a58f0197403
EnZzzzzz <957078942@qq.com>
2022-02-11 21:59:48 +0800

    .

--- main.py
+++ main.py
@@ -14,33 +14,35 @@ flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image s
 flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
 FLAGS = flags.FLAGS
 
+
 def main(_):
-  pp.pprint(flags.FLAGS.__flags)
+    pp.pprint(flags.FLAGS.__flags)
 
-  if not os.path.exists(FLAGS.checkpoint_dir):
-    os.makedirs(FLAGS.checkpoint_dir)
-  if not os.path.exists(FLAGS.sample_dir):
-    os.makedirs(FLAGS.sample_dir)
+    if not os.path.exists(FLAGS.checkpoint_dir):
+        os.makedirs(FLAGS.checkpoint_dir)
+    if not os.path.exists(FLAGS.sample_dir):
+        os.makedirs(FLAGS.sample_dir)
 
-  #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-  run_config = tf.ConfigProto()
-  run_config.gpu_options.allow_growth=True
+    # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    run_config = tf.ConfigProto()
+    run_config.gpu_options.allow_growth = True
 
-  with tf.Session(config=run_config) as sess:
-    layoutgan = LAYOUTGAN(sess,
-      batch_size=FLAGS.batch_size,
-      sample_num=FLAGS.batch_size,
-      dataset_name=FLAGS.dataset,
-      checkpoint_dir=FLAGS.checkpoint_dir,
-      sample_dir=FLAGS.sample_dir)
+    with tf.Session(config=run_config) as sess:
+        layoutgan = LAYOUTGAN(sess,
+                              batch_size=FLAGS.batch_size,
+                              sample_num=FLAGS.batch_size,
+                              dataset_name=FLAGS.dataset,
+                              checkpoint_dir=FLAGS.checkpoint_dir,
+                              sample_dir=FLAGS.sample_dir)
 
-    show_all_variables()
+        show_all_variables()
 
-    if FLAGS.train:
-      layoutgan.train(FLAGS)
-    else:
-      if not layoutgan.load(FLAGS.checkpoint_dir)[0]:
-        raise Exception("[!] Train a model first, then run test mode")
+        if FLAGS.train:
+            layoutgan.train(FLAGS)
+        else:
+            if not layoutgan.load(FLAGS.checkpoint_dir)[0]:
+                raise Exception("[!] Train a model first, then run test mode")
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
